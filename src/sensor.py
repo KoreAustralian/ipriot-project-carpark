b50c2d66baa266c6ddbe01825f231c054a32bf91
20115830 <20115830@tafe.wa.edu.au>
2024-06-11 08:05:26 +0800

    implemented sensor detection

--- src/sensor.py
+++ src/sensor.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+import random
 class Sensor:
     """Provides sensors to detct cars"""
     def __init__(self,
@@ -9,12 +11,33 @@ class Sensor:
         self.car_park = car_park
         self.is_active = is_active
 
+    def _scan_plate(self):
+        return "FAKE-" + format(random.randint(0, 999), '03d')
+
+    @abstractmethod
+    def update_car_park(self,  plate):
+        raise NotImplementedError
+
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
     def __str__(self):
         return f'{self.id}: Sensor is {"is active" if self.is_active else "if active"}'
 
 class EntrySensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    ...
+    def _scan_plate(self):
+        # Fudge: just so we can demonstrate scan on exit...
+        return random.choice(self.car_park.plates)
+
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+
 
